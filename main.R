4ff223843c1309ccf4f5af26be7d875c3ff64d2d
Corrod3 <ohne.realitaet@posteo.de>
2017-02-07 17:38:25 +0100

    update

--- main.R
+++ main.R
@@ -42,34 +42,75 @@ rm(p, packages)
 ################################################
 
 # Import responses
-SPFT <- read.csv2("raw/SPFT-20170207-backup.csv", sep = ",")
+SPFT.Main <- read.csv2("raw/SPFT-20170207-backup.csv", sep = ",")
 SPFT.MTurk <- read.csv2("raw/SPFT_MTurk_20170207.csv", sep = ",")
 
 
 # Import realized outcomes
 FQ <- read_excel("raw/SPFT-questions-test.xlsx")
 
+# import MTurk batch results
+MTurk.batch <- read.csv2("raw/Batch_2677782_batch_results.csv", sep = ",")
+MTurk.batch <- MTurk.batch %>% select(id.mturk = WorkerId, WorkTimeInSeconds, Answer.surveycode)
+
 ###############################################
 # 2. Clean Data
 ###############################################
 
 # Merge SPFT and SPFT Turk
+SPFT <- dplyr::bind_rows(SPFT.Main, SPFT.MTurk[-(1:2),])
+
 
 # Remove unfinished surveys
 SPFT <- SPFT %>% dplyr::filter(Finished == "True")
-SPFT.MTurk <- SPFT.MTurk %>% dplyr::filter(Finished == "True")
 
 # Change variable types ########################
 
 # Dates
 SPFT$StartDate <- as.Date(as.character(SPFT$StartDate))
 SPFT$EndDate <- as.Date(as.character(SPFT$EndDate))
-
-SPFT.MTurk$StartDate <- as.Date(as.character(SPFT.MTurk$StartDate))
-SPFT.MTurk$EndDate <- as.Date(as.character(SPFT.MTurk$EndDate))
+SPFT$Duration..in.seconds. <- as.numeric(SPFT$Duration..in.seconds.)
 
 # Remove test surveys
-SPFT.
+SPFT <- SPFT %>% filter(StartDate > "2017-02-05")
+
+# Drop empthy forecasts (Mainly MTurk failed attention check)
+SPFT <- SPFT %>% filter(time.sec2_First.Click != "")
+
+# delete unnecessary information
+SPFT <- SPFT %>% select(-Status, -IPAddress, -Progress, -Finished,
+                        -RecipientLastName, -RecipientFirstName, 
+                        -RecipientEmail, -LocationLatitude, -LocationLongitude,
+                        -DistributionChannel, -StartDate, ExternalReference,
+                        -further.info...Topics, -contains("Click") )
+# delete raw files
+rm(SPFT.Main, SPFT.MTurk)
+
+####################################################
+# Identify problematic MTurk users 
+###################################################
+
+# only MTurk
+MTurk <- SPFT %>% filter(id.mturk != "")
+
+# Criterion 1: Check failed attention checks
+MTurk$c1 <- 0
+MTurk[MTurk$att.check != 48,]$c1 <- 1
+
+# Criterion 2: All BNT questions wrong
+MTurk <- MTurk %>% 
+          mutate(c2 = ifelse(bnt1 != 30 & bnt2 != 25 & bnt3 != 20 & bnt4 != 50,
+                             1, 0))
+
+# Criterion 3: Short time
+# summary(MTurk$Duration..in.seconds.)
+MTurk <- MTurk %>% mutate(c3 = ifelse(Duration..in.seconds. < 600, 1, 0))
+
+MTurk.Prob <- MTurk %>% select(ResponseId, id.mturk, CompletionCode, c1, c2, c3) %>% filter(c1 == 1 & c2 == 1 & c3 == 1)
+
+# merge dataframe
+MTurk <- merge(MTurk, MTurk.batch, by = "id.mturk", all = TRUE)
+
 
 
 ################################################
